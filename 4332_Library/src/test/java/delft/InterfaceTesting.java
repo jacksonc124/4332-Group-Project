10e96be676676bf19663a73722084cd10c3665c0
Alex <apurce5@lsu.edu>
2025-04-13 19:24:34 -0500

    Interface Testing File
    This is so I can switch to Intellij rq

---
+++ 4332_Library/src/test/java/delft/InterfaceTesting.java
@@ -0,0 +1,16 @@
+package delft;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import net.jqwik.api.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class InterfaceTesting {
+    
+}
