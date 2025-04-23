e73b316929e473eba9c2cf07043ef0b5de959d9b
Eby <133599375+Eberechi-N@users.noreply.github.com>
2025-04-22 21:47:23 -0500

    finished LibraryAccounts implementation

--- 4332_Library/src/main/java/delft/LibraryAccounts.java
+++ 4332_Library/src/main/java/delft/LibraryAccounts.java
@@ -1,5 +1,47 @@
 package delft;
 
+import static org.mockito.ArgumentMatchers.booleanThat;
+
 public class LibraryAccounts {
-    
+
+    private double cashBalance = 39000;
+
+    // allow librarian to add cash to balance at any point
+    public void addCashBalance(double cash) {
+        cashBalance += cash;
+    }
+
+    // allow librarian to withdraw Salary from balance
+    public boolean withdrawSalary(double salary) {
+        if (cashBalance >= salary) {
+            cashBalance -= salary;
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // orders new books from purchasing. deducted from cash balance
+    public boolean buyBooks(String bookTitle, String bookID) {
+        if (bookID != null && bookTitle != null) {
+            Purchasing orders = new Purchasing();
+            double bookCost = orders.processPurchase(bookTitle);
+
+            // deduct only if enough money
+            if (cashBalance >= bookCost) {
+                cashBalance -= bookCost;
+                return true;
+            } else {
+                System.out.println("Insufficient fund in balance");
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    public double getCashBalance() {
+        return cashBalance;
+    }
+
 }
